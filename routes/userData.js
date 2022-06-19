b4f0b530d1620855744a10f3f8452610baf7f59a
cosmas Thuku <cosmasbachia@equitybank.co.ke>
2022-06-20 02:46:20 +0300

    users functionality for crude ops in an array

---
+++ routes/userData.js
@@ -0,0 +1,18 @@
+export let users1 = [
+    {
+        "firstname":"john",
+        "lastname":"doe",
+        "age":45
+    },
+    {
+        "firstname":"jane",
+        "lastname":"doe",
+        "age":35
+    },
+    {
+        "firstname":"june",
+        "lastname":"doe",
+        "age":25
+    }
+];
+ 
